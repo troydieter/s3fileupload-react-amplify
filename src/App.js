396f246fd1031b76b3ed48d8962e834ab39dd318
Troy Dieter <troydieter@gmail.com>
2022-01-03 12:46:52 -0500

    modified text

--- src/App.js
+++ src/App.js
@@ -51,16 +51,16 @@ const App = () => {
   return (
     <AmplifyAuthenticator>
       <AmplifySignIn
-        headerText='AWS TCS Team, Sign-In with Your E-Mail Address'
+        headerText='Sign-In with Your E-Mail Address - Feedback Processor'
         slot='sign-in'
       />
       <AmplifySignUp
-        headerText='AWS TCS Team, Sign-Up with Your Valid E-Mail Address'
+        headerText='Sign-Up with Your Valid E-Mail Address - Feedback Processor'
         slot='sign-up'
       />
       <div className='header'>
         <h2>
-          <a href='/'>TCS Team Video Uploader</a>
+          <a href='/'>Feedback Processor</a>
         </h2>
       </div>
       <div className='video-uploader'>
