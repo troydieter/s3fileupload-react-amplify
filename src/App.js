3d4f8a0605bfd8c83036ddf5973213103b242111
Troy Dieter <troydieter@gmail.com>
2022-01-03 13:13:22 -0500

    file extensions

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ const App = () => {
               className='video-input'
               type='file'
               id='file-input'
-              accept='image/*, video/*'
+              accept='.csv,.txt,.pdf,.json,.yaml,.doc,.docx'
               onChange={(e) => onChange(e)}
             />
           </p>
